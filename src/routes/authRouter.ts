1648909401a288c3547f1dd4893d0768965bbcc3
Francisco <francisco.carvalho.q@gmail.com>
2023-12-01 20:27:36 -0300

    refactor(authRouter): removendo codigo que envia cookie com a resposta
    motivo: o frontend nunca chega a de fato usar o cookie enviado na
    resposta, pois a API Fetch ignora silenciosamente cookies enviados de
    outro site.
    
    veja: https://discourse.mozilla.org/t/fetch-will-not-accept-cross-site-foreign-cookies/48616

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -4,22 +4,12 @@ import { __prod__ } from "../constants";
 
 const authRouter = Router();
 
-const setToken = (res: any, token: string) => {
-  res.cookie("token", token, {
-    maxAge: 3600000,
-    httpOnly: false,
-    sameSite: "none",
-    secure: __prod__,
-  });
-}
-
 authRouter.post("/signup", async (req, res) => {
   try {
     const clone = { ...req.body };
     const { password } = clone;
     const user = await AuthController.signup(clone);
     const { token } = await AuthController.signin(user.email, password);
-    setToken(res, token);
     res.json({ data: { auth: true, user, token } });
   } catch (err: any) {
     res.status(400).json({ error: { message: err.message } });
@@ -30,7 +20,6 @@ authRouter.post("/signin", async (req, res) => {
   try {
     const clone = { ...req.body };
     const { token, userData: user } = await AuthController.signin(clone.email, clone.password);
-    setToken(res, token);
     res.json({ data: { auth: true, user, token } });
   } catch (err: any) {
     res.status(401).json({ error: { message: err.message } });
@@ -39,7 +28,6 @@ authRouter.post("/signin", async (req, res) => {
 
 authRouter.post("/signout", async (req, res) => {
   try {
-    res.clearCookie("token");
     res.status(201).json({ data: { auth: false, user: null, token: null } });
   } catch (err: any) {
     res.status(401).json({ error: { message: err.message } });
