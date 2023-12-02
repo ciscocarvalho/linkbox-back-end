99a484b9f1b95709b8653e85781e02dcbbc72845
Francisco <francisco.carvalho.q@gmail.com>
2023-12-01 21:13:20 -0300

    fix(middlewares/isAuthenticated): corrigindo formato errado de resposta

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
   if (!req.session?.authenticated) {
-    res.status(404).json({ msg: "No current user authenticated" });
+    res.status(404).json({ error: { message: "No current user authenticated" } });
     return;
   }
 
